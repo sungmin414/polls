babf7e76b7caf23ad6569c98804b28504ca506f5
sungmin414 <sungmin3231@gmail.com>
2018-08-23 18:38:48 +0900

    admin 필드순서, 분리해서보기, 접어서보기 설정코드작성

--- app/polls/admin.py
+++ app/polls/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
 from .models import Question, Choice
 
-admin.site.register(Question)
+# admin 필드 순서 변경
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+
+# 각 필드 분리해서 보기
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         ('Question Statement', {'fields': ['question_text']}),
+#         ('Date Information', {'fields': ['pub_date']}),
+#     ]
+
+
+# 필드 접어서보기(date information 에만 설정했음
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+                ('Question Statement', {'fields': ['question_text']}),
+                ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+            ]
+
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
