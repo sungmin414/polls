3fb8684562d74b42025abedbe8d7fb65ed2426e0
sungmin414 <sungmin3231@gmail.com>
2018-08-23 19:44:45 +0900

    픽터 사이드바, 검색 박스 추가 list_filter, search_fields

--- app/polls/admin.py
+++ app/polls/admin.py
@@ -40,7 +40,9 @@ class QuestionAdmin(admin.ModelAdmin):
                     ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
                 ]
     inlines = [ChoiceInline]    # Choice 모델 클래스 같이 보기
-    list_display = ('question_text', 'pub_date')    # 레코드 리스트 항목 지정
+    list_display = ('question_text', 'pub_date')    # 레코드 리스트 항목 지
+    list_filter = ['pub_date']  # 필터 사이드 바 추가
+    search_fields = ['question_text']   # 검색 박스 추가
 
 
 
